c58b208f9bced73fc50e594664d4b83fd318a2d7
Eric Chen <ericchen201896@gmail.com>
2021-01-29 14:40:02 -0800

    eslint error fixes

--- routes/player-api-routes.js
+++ routes/player-api-routes.js
@@ -1,32 +1,29 @@
 // Requiring our models and passport as we've configured it
 const db = require("../models");
-const passport = require("../config/passport");
 
-module.exports = function (app) {
-    // Add a new land
-    app.post("/api/player", (req, res) => {
-        db.Player.create(req.body).then((dbPlayers) => {
-            res.json(dbPlayers);
-        });
+module.exports = function(app) {
+  // Add a new land
+  app.post("/api/player", (req, res) => {
+    db.Player.create(req.body).then(dbPlayers => {
+      res.json(dbPlayers);
     });
+  });
 
-    // Get all lands
-    app.get("/api/", (req, res) => {
-        db.Player.findAll({
-        }).then((dbPlayers) => {
-            res.json(dbPlayers);
-        });
+  // Get all lands
+  app.get("/api/", (req, res) => {
+    db.Player.findAll({}).then(dbPlayers => {
+      res.json(dbPlayers);
     });
+  });
 
-    // Route for updating land data
-    app.put("/api/player/:id", (req, res) => {
-        db.Player.update(req.body,
-            {
-                where: {
-                    id: req.params.id
-                }
-            }).then((dbPlayers) => {
-                res.json(dbPlayers)
-            })
+  // Route for updating land data
+  app.put("/api/player/:id", (req, res) => {
+    db.Player.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    }).then(dbPlayers => {
+      res.json(dbPlayers);
     });
+  });
 };
