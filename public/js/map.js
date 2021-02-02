566a92570f3d12ac9397cfabe070b427804a726e
Eric Chen <ericchen201896@gmail.com>
2021-02-01 20:54:28 -0800

    Added in ripped cloth animation

--- public/js/map.js
+++ public/js/map.js
@@ -10,7 +10,7 @@ function createMap() {
   map = new Datamap({
     element: document.getElementById("mapDiagram"),
     scope: "usa",
-    done: function(datamap) {
+    done: function (datamap) {
       console.log(datamap);
       datamap.svg.selectAll(".datamaps-subunit").on("click", geography => {
         changeOwnership(geography);
@@ -20,7 +20,7 @@ function createMap() {
     },
     geographyConfig: {
       highlightBorderColor: "#bada55",
-      popupTemplate: function(land, data) {
+      popupTemplate: function (land, data) {
         const i = `<div class="hoverinfo">${land.properties.name}
         Resource Cost: ${data.resourceCost} `;
         return i;
@@ -29,7 +29,6 @@ function createMap() {
       highlightBorderWidth: 3
     },
     fills: {
-      // notBrought: "#D3D3D3",
       [broughtByPlayer]: "#228B22",
       [broughtByOpponent]: "#B22222",
       defaultFill: "#D3D3D3"
@@ -47,20 +46,11 @@ function changeOwnership(geography) {
 
 function setLandColor(geography) {
   const stateTarget = geography.id;
-  if (States[stateTarget].isOwned === true) {
-    if (States[stateTarget].ownedBy === "Rockefeller") {
-      States[stateTarget].fillKey = broughtByPlayer;
-    } else {
-      States[stateTarget].fillKey = broughtByOpponent;
-    }
+  if (States[stateTarget].isOwned) {
+    States[stateTarget].fillKey = broughtByPlayer;
   } else {
     States[stateTarget].fillKey = "defaultFill";
   }
 
-  console.log(States[stateTarget].fillKey);
-  // map.updateChoropleth(
-  //   { [stateTarget]: { fillKey: States[stateTarget].fillKey } },
-  //   { reset: false }
-  // );
   map.updateChoropleth(States, { reset: true });
 }
